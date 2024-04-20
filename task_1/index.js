5d3f928fa275fc97338691476923d0323a42c021
Margo <plokhova.margarita@yandex.ru>
2024-04-20 18:53:42 +0300

    finished first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,30 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+let encodedIds = [];
 
+const decoded = (encoded, translations) => {
+	const decodedArray = encoded.map((item) => {
+		const decodedObject = {};
+		for (let key in item) {
+			if (key.includes("Id") && key != "groupId") {
+				encodedIds.push(item[key]);
+				for (let newKey in translations) {
+					if (item[key] == newKey) {
+						decodedObject[key] = translations[newKey];
+					}
+				}
+			} else {
+				decodedObject[key] = item[key];
+			}
+		}
+		return decodedObject;
+	});
+	return decodedArray;
+};
 
-
-// console.log(decoded)
+decoded(encoded, translations);
+const findUniqueIds = (value, index, array) => {
+	return array.filter((item) => item === value).length === 1;
+};
+const uniqueIds = encodedIds.filter(findUniqueIds);
+console.log(uniqueIds);
