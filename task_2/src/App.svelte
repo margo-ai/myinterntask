4fd30d61e0a0ec77456db3ca563eed9bf9cc526d
Margo <plokhova.margarita@yandex.ru>
2024-04-21 23:14:17 +0300

    finished second task

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,107 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script	async script lang="ts">
+
+  import { fixToTwoDigits } from './lib/utilities/functions';
+  import {onMount} from 'svelte';
+  import icon from './assets/transfer_icon.svg'
+
+
+  const BASE_URL: string = 'https://open.er-api.com/v6/latest'
+  // изначально выбранная основная валюта
+  let baseCode: string = "USD";
+
+  // список валют
+  let currencies: string[] = [];
+
+  type TRates = {
+    [key: string]: number;
+  }
+  // курсы по валютам
+  let rates: TRates;
+
+  // выбранные валюты
+  let fromCurrencySelected: string = baseCode;
+  let toCurrencySelected: string;
+
+  // флаг отрисовки компонента
+  let ready = false;
+
+  // курс по текущей паре валют
+  let exchangeRate: number;
+
+  // введенные суммы
+  let fromAmount: number = 1;
+  $:toAmount = fixToTwoDigits(fromAmount * exchangeRate);
+  
+  
+  const fetchData = async () => {
+    let response = await fetch(`${BASE_URL}/${baseCode}`);
+    let data = await response.json();
+   
+    for (let key in data.rates) {
+      currencies.push(key)    
+    }
+    rates = data.rates;
+    
+    const firstCurrency = Object.keys(data.rates)[1];
+    toCurrencySelected = !toCurrencySelected ? firstCurrency : toCurrencySelected;
+    exchangeRate = data.rates[toCurrencySelected];
+      
+    ready = true;
+  }
+
+  onMount(() => {
+    fetchData();
+  });
+
+
+  const handleSecondInput = () => {
+    fromAmount = fixToTwoDigits(toAmount / exchangeRate);
+  }
+
+  const handleFirstSelect = () => {
+    baseCode = fromCurrencySelected;
+    fetchData();
+  }
+
+  const handleSecondSelect = () => {
+    exchangeRate = rates[toCurrencySelected];
+  }
+
 </script>
 
 <main>
+  <h1>Currency converter</h1>
+  <div class="converter">
+    <div>
+    <input type="number" bind:value={fromAmount} />
+    <select bind:value={fromCurrencySelected} on:change={handleFirstSelect}>
+    {#if ready}
+      {#each currencies as currency}
+			<option value={currency}>
+				{currency}
+			</option>
+		{/each}
+    {/if}
+  </select>
+  </div>
+  <div class="equal">
+    <img src={icon} alt="Transfer icon">
+  </div>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <input type="number" bind:value={toAmount} on:input={handleSecondInput}/>
+    <select bind:value={toCurrencySelected} on:change={handleSecondSelect}>
+    {#if ready}
+      {#each currencies as currency}
+			<option value={currency}>
+				{currency}
+			</option>
+		{/each}
+    {/if}
+    </select>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
